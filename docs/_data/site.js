77af6fb35e1e85914b79df7be5f215e252320eb3
Guillem Cordoba <guillem.cordoba@gmail.com>
2020-10-27 20:41:28 +0100

    Attempt logso

---
+++ docs/_data/site.js
@@ -0,0 +1,16 @@
+module.exports = async function () {
+  return {
+    name: "Holochain Gym",
+    description:
+      "Holochain Gym contains exercises, resources and trainings to learn and ",
+    socialLinks: [
+      {
+        name: "GitHub",
+        url: "https://github.com/holochain-gym/",
+      },
+    ],
+    helpUrl: "https://github.com/holochain-gym/",
+    logoAlt: "O with occluded right edge to appear also as a C",
+    socialMediaImage: "/_assets/social-media-image.jpg",
+  };
+};
